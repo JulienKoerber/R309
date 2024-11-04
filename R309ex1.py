57b17812546e2cd1ec7a4b83674a1675bb64d9f3
e2301028 <julien.koerber22@gmail.com>
2024-11-04 14:48:02 +0100

    V2

--- R309ex1.py
+++ R309ex1.py
@@ -1,6 +1,27 @@
-def divEntier(x: int, y:int) -> int:
+def divEntier(x: int, y: int) -> int:
+    if y == 0:
+        raise ZeroDivisionError("La division par zéro n'est pas permise.")
+
     if x < y:
         return 0
-    else: 
+    else:
         x = x - y
         return divEntier(x, y) + 1
+def main():
+    try:
+        x = int(input("x : "))
+        y = int(input("y : "))
+
+        if y <= 0:
+            print("La valeur de y doit être positive")
+            return
+
+        resultat = divEntier(x, y)
+        print(f"La division de {x} par {y} est : {resultat}")
+
+    except ValueError:
+        print("entrez un nombre entier")
+    except ZeroDivisionError as e:
+        print(e)
+if __name__ == "__main__":
+    main()
