26f2e5fa88ff16eb42a7a454270faf73b1964eb9
julie <julien.koerber22@gmail.com>
2024-11-13 09:02:16 +0100

    interface

--- Exercices/R309ex2.py
+++ Exercices/R309ex2.py
@@ -1,16 +1,11 @@
-# 1. Définir le nom du fichier dans une variable
 nom_fichier = "testex2.txt"
 
-# 2. Essayer d'ouvrir et de lire le fichier
 try:
-    # Ouverture du fichier en lecture avec le mot-clé 'with'
     with open(nom_fichier, 'r') as fichier:
-        # Lecture et affichage des lignes
         for ligne in fichier:
-            ligne = ligne.rstrip("\n\r")  # Retire les retours à la ligne
+            ligne = ligne.rstrip("\n\r")
             print(ligne)
 
-# 3. Gestion des exceptions
 except FileNotFoundError:
     print("Erreur : Le fichier spécifié est introuvable.")
 except IOError:
@@ -20,6 +15,5 @@ except FileExistsError:
 except PermissionError:
     print("Erreur : Vous n'avez pas les permissions nécessaires pour accéder au fichier.")
 
-# 4. Bloc finally pour indiquer la fin du programme
 finally:
     print("Fin du programme.")
